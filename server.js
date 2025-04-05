44f449c9812b9fa917229c5ce30d971b392b8186
caiorn <caio.ssn96@gmail.com>
2025-04-05 16:03:58 -0300

    routers test

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const UserRouters = require('./routes/UserRouters.js');
 
 // Carrega o arquivo .env específico com base na variável de ambiente
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const envVars = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+};
 
-const envVars = Object.keys(process.env).reduce((acc, key) => {
-    acc[key] = process.env[key];
-    return acc;
-  }, {});
+app.use(express.json());
+app.use('/api', UserRouters);
+
 
 app.get('/', (req, res) => {
-  res.send(`${JSON.stringify(envVars, null, 2)}`);
-  //res.send(`Rodando na porta ${PORT} - Ambiente: ${process.env.NODE_ENV}<br>${JSON.stringify(envVars, null, 2)}`);
+  console.log(req.body);
+  res.json(envVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}, Ambiente: ${process.env.NODE_ENV}`);
 });
+
